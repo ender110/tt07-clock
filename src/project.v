2f7958b48223201deaa7b898bd3c51260ab5b0b9
ender110 <153929747+ender110@users.noreply.github.com>
2024-05-01 21:30:22 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,51 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-assign uo_out=ui_in;
-	assign uio_out=uio_in;
-	assign uio_oe=uio_in;
+
+assign uio_out=uio_in;
+assign uio_oe=uio_in;
+wire reset;
+wire clock ;
+assign clock=clk;
+assign reset=rst_n;
+
+reg[15:0]clock_counter;
+always @(posedge clock or negedge reset)
+begin
+	if(!reset)
+	begin
+		clock_counter<=16'd0;
+	end
+	else
+	begin
+		clock_counter<=clock_counter+1'd1;
+	end
+end
+
+wire second_flag;
+assign second_flag=clock_counter[15:0]==16'd0;
+reg [5:0]second;
+	//second
+always @(posedge clock or negedge reset)
+begin
+	if(!reset)
+	begin
+		second<=0;
+	end
+	else
+	begin
+		if(second_flag)
+		begin
+			if(second==6'd59)
+			begin
+				second<=6'd0;
+			end
+			else
+			begin
+				second<=second+1'd1;
+			end
+		end
+	end
+end
+	assign uo_out=second[6:0];
 endmodule
