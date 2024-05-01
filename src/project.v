09a1dcbb13e4c4b3c20a66bd5e8cdd7240f70423
ender110 <153929747+ender110@users.noreply.github.com>
2024-05-02 02:27:07 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-	reg [0]clock_run_flag=1;
+	reg clock_run_flag=1;
 	assign uio_out=uio_in+ui_in+{7'd0,ena};
 	assign uio_oe[7:4]=uio_in[7:4]+uio_in[3:0];
 wire reset;
