ba4e1759313f75692f62cbe2582f9799808ee35f
ender110 <153929747+ender110@users.noreply.github.com>
2025-03-29 06:21:57 +0000

    a

--- test/test.py
+++ test/test.py
@@ -23,9 +23,14 @@ async def test_project(dut):
     dut.rst_n.value=0;
     for i in range(10):
         dut.i_encoder_a.value=1-dut.i_encoder_a.value;
-        await ClockCycles(dut.clk, 5)
+        await ClockCycles(dut.clk, 2)
+        assert dut.o_step_back.value == 1
+        await ClockCycles(dut.clk, 2)
+        assert dut.o_step_back.value == 0
         dut.i_encoder_b.value=1-dut.i_encoder_b.value;
-        await ClockCycles(dut.clk, 5)
+        assert dut.o_step_back.value == 1
+        await ClockCycles(dut.clk, 2)
+        assert dut.o_step_back.value == 0
     # # Reset
     # dut._log.info("Reset")
     # dut.ena.value = 1
